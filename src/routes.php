9a017fac3e72b347af04196b1e5f50d8514d13c8
Denis Raygorodskiy <raygorodskij@rapidapi.com>
2017-08-30 11:41:03 +0300

    fix schema name

--- src/routes.php
+++ src/routes.php
@@ -3,17 +3,17 @@ $routes = [
     'metadata',
     'getRequestToken',
     'getAccessToken',
-    'createItem', //1
-    'archiveItem', // 1
-    'unarchiveItem', // 1
-    'addFavoriteItem', //1
-    'removeFavoriteItem', // 1
-    'deleteItem', // 1
-    'addTagsToItem', // 1
-    'removeTagsFromItem', // 1
-    'replaceTagsFromItem', // 1
-    'removeAllTags', // 1
-    'renameTag', // 1
+    'createItem',
+    'archiveItem',
+    'unarchiveItem',
+    'addFavoriteItem',
+    'removeFavoriteItem',
+    'deleteItem',
+    'addTagsToItem',
+    'removeTagsFromItem',
+    'replaceTagsFromItem',
+    'removeAllTags',
+    'renameTag',
     'getAllItems',
     'postMultipleActions'
 ];
